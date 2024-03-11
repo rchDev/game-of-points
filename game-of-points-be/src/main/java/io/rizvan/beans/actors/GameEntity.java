62ca742f6b2abf0f979af6af8223bd8ab319b11e
rizvanium <rizvan.chalilovas@proton.me>
2024-03-11 22:56:50 +0200

    [backend]: add id to every game entity.

--- game-of-points-be/src/main/java/io/rizvan/beans/actors/GameEntity.java
+++ game-of-points-be/src/main/java/io/rizvan/beans/actors/GameEntity.java
@@ -3,11 +3,13 @@ package io.rizvan.beans.actors;
 import io.rizvan.beans.HitBox;
 
 public abstract class GameEntity {
+    protected String id;
     protected double x;
     protected double y;
     protected HitBox hitBox;
 
     public GameEntity(double x, double y, int width, int height) {
+        this.id = java.util.UUID.randomUUID().toString();
         this.x = x;
         this.y = y;
         this.hitBox = new HitBox(width, height);
