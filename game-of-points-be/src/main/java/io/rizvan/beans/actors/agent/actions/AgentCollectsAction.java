e271bf8c0acf5be13bd069e776b1fa2371d312a2
rizvanium <rizvan.chalilovas@proton.me>
2024-04-06 00:06:31 +0300

    [backend]: implement resource point collection behaviour

--- game-of-points-be/src/main/java/io/rizvan/beans/actors/agent/actions/AgentCollectsAction.java
+++ game-of-points-be/src/main/java/io/rizvan/beans/actors/agent/actions/AgentCollectsAction.java
@@ -1,10 +1,19 @@
 package io.rizvan.beans.actors.agent.actions;
 
 import io.rizvan.beans.GameState;
+import io.rizvan.beans.ResourcePoint;
 
 public class AgentCollectsAction extends AgentAction {
+    private final ResourcePoint resource;
+
+    public AgentCollectsAction(ResourcePoint resource) {
+        this.resource = resource;
+    }
+
     @Override
     public void apply(GameState gameState) {
-
+        var agent = gameState.getAgent();
+        agent.setPoints(agent.getPoints() + resource.getPoints());
+        gameState.removeResource(resource.getId());
     }
 }
