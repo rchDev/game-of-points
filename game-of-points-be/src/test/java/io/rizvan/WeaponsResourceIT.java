28191ff3feb2ad676033936b4c05c06888e4df58
rchDev <rizvan.chalilovas@proton.me>
2025-04-17 12:16:48 +0300

    Remove a bunch of useless tests

--- game-of-points-be/src/test/java/io/rizvan/WeaponsResourceIT.java
+++
@@ -1,8 +0,0 @@
-package io.rizvan;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class WeaponsResourceIT extends WeaponsResourceTest {
-    // Execute the same tests but in packaged mode.
-}
