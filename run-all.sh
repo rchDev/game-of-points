8944b798931f41be7c452b432931743f62917638
rchDev <rizvan.chalilovas@proton.me>
2025-04-18 20:41:49 +0300

    update run-all.sh to work again after python changes

--- run-all.sh
+++ run-all.sh
@@ -9,37 +9,35 @@ NGROK_PID=
 trap 'echo "🧹 Cleaning up..."; kill $PY1_PID $PY2_PID $QUARKUS_PID $NGROK_PID 2>/dev/null || true' EXIT
 
 cd game-of-points-be/src/main/java/io/rizvan/beans/actors/agent
-source ./venv-3.12/bin/activate
-python ./baysian_network.py > baysian_network.log 2>&1 &
+poetry run python ./baysian_network.py > baysian_network.log 2>&1 &
 PY1_PID=$!
 
 cd ./sentiment-analysis
-source ./venv-3.12/bin/activate
-python ./sentiment_classifier.py > sentiment_classifier.log 2>&1 &
+poetry run python ./sentiment_classifier.py predict > sentiment_classifier.log 2>&1 &
 PY2_PID=$!
 
 echo "⏳ Launching bayes-net and sentiment classifier services..."
 while ! nc -z localhost 25334; do sleep 0.5; done
 while ! nc -z localhost 25336; do sleep 0.5; done
-echo "✅ Services are ready!"
+echo "✅ Bayes-net and sentiment classifier are ready!"
 
 cd ../../../../../../../../../
 ./mvnw clean install
 nohup ./mvnw quarkus:dev > quarkus_main_server.log 2>&1 &
 QUARKUS_PID=$!
 
-echo "⏳ Launching Quarkus..."
+echo "⏳ Launching game backend..."
 while ! nc -z localhost 8080; do sleep 0.5; done
-echo "✅ Quarkus is ready."
+echo "✅ Game backend is ready."
 
 # 🌐 Start ngrok
-nohup ngrok http 8080 > ngrok.log 2>&1 &
+nohup ngrok http --domain=rizvan.ngrok.dev 8080 > ngrok.log 2>&1 &
 NGROK_PID=$!
 
 sleep 1
 
 cd ../game-of-points-fe
-echo "⏳ Launching Front end..."
+echo "⏳ Launching game frontend..."
 npm run dev
 while ! nc -z localhost 5173; do sleep 0.5; done
-echo "✅ Front end is ready"
+echo "✅ Game frontend is ready"
